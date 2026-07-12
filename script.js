92b30c40a9089cbee8b5497b6eb99230f75ff4c4
sakina <sakinazehrapk@gmail.com>
2026-07-13 03:06:43 +0500

    added first scene

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const startBtn = document.getElementById("startBtn");
+const mainMenu=document.getElementById("mainMenu");
+const storyScreen=document.getElementById("storyScreen");
+const startBtn=document.getElementById("startBtn");
+const continueBtn=document.getElementById("continueBtn");
 startBtn.addEventListener("click",()=>{
-    alert("Game Starts!");
+    mainMenu.classList.add("hidden");
+    storyScreen.classList.remove("hidden");
+});
+continueBtn.addEventListener("click",()=>{
+    alert("Next scene coming soon!");
 });
