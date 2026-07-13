1a5ead19b3fb1aef337fbb893278dcc7f3eaa364
sakina <sakinazehrapk@gmail.com>
2026-07-13 15:36:09 +0500

    asking players to name their chracter

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ const speaker=document.getElementById("speaker");
 const dialogue=document.getElementById("dialogue");
 let scenes=[];
 let currentScene = "intro_1";
+const nameBox=document.getElementById("nameBox");
+const heroName=document.getElementById("heroName");
+const saveName=document.getElementById("saveName");
+let player={
+    name:"Hero"
+};
+
 startBtn.addEventListener("click", async()=>{
     mainMenu.classList.add("hidden");
     storyScreen.classList.remove("hidden");
@@ -44,10 +51,24 @@ function loadScene(){
     dialogue.textContent = scene.text;
     choices.classList.add("hidden");
     continueBtn.classList.remove("hidden");
+    nameBox.classList.add("hidden");
     if(scene.choices){
         continueBtn.classList.add("hidden");
         choices.classList.remove("hidden");
         choice1.textContent = scene.choices[0].text;
         choice2.textContent = scene.choices[1].text;
     }
+    if(scene.id==="name_hero"){
+    continueBtn.classList.add("hidden");
+    choices.classList.add("hidden");
+    nameBox.classList.remove("hidden");
 }
+}
+saveName.addEventListener("click",()=>{
+    if(heroName.value.trim()===""){
+        alert("Please enter a name.");
+        return;
+    }
+    player.name=heroName.value.trim();
+    alert("Welcome, " + player.name + "!");
+});
